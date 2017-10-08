6bb716a4b9f6b5721ce67d20c308e7a71c4d55ef
park kyung suk <boardguy1024@gmail.com>
2017-10-08 11:49:46 +0900

    spanで表示

--- Mapkit_Display/ViewController.swift
+++ Mapkit_Display/ViewController.swift
@@ -24,18 +24,22 @@ class ViewController: UIViewController {
         
         //위도 경도를 사용해서 중심부를 지정
         
-        let lat: CLLocationDegrees = 37.506804
-        let Lon: CLLocationDegrees = 139.930531
+        let lat: CLLocationDegrees = 35.648984
+        let Lon: CLLocationDegrees = 139.551272
         //WGS84(세계측지계)를 이용해서 경도,위도를 이용해 지리적좌표의 스트럭쳐를 구함
         let centerCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, Lon)
+//        
+//        //중심부로부터 범위를 설정
+//        let latDistanceMeter: CLLocationDistance = 500
+//        let lonDistanceMeter: CLLocationDistance = 500
+//        
+//        // reqion(지역)을 작성
+//        let region: MKCoordinateRegion =
+//            MKCoordinateRegionMakeWithDistance(centerCoordinate, latDistanceMeter, lonDistanceMeter)
         
-        //중심부로부터 범위를 설정
-        let latDistanceMeter: CLLocationDistance = 500
-        let lonDistanceMeter: CLLocationDistance = 500
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
         
-        // reqion(지역)을 작성
-        let region: MKCoordinateRegion =
-            MKCoordinateRegionMakeWithDistance(centerCoordinate, latDistanceMeter, lonDistanceMeter)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: centerCoordinate, span: span)
         
         // reqion을 설정
         myMapView.setRegion(region, animated: true)
